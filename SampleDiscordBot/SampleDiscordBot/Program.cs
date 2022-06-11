fca26dd5976a7c7e35f1f8a6761e8250a0bceed4
Anthony Raiti <33672701+antraiti@users.noreply.github.com>
2022-06-11 10:28:50 -0400

    Added project files

---
+++ SampleDiscordBot/SampleDiscordBot/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+using System.Net;
+
+namespace SampleDiscordBot
+{
+	public class Program
+	{
+		private DiscordSocketClient _client;
+		private CommandHandler _commandHandler;
+		private CommandService _commandService;
+
+		private readonly string _token = "YOUR_TOKEN_HERE"; 
+
+		static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult(); 
+
+		public async Task MainAsync()
+		{
+			Console.WriteLine("starting"); 
+			// When working with events that have Cacheable<IMessage, ulong> parameters,
+			// you must enable the message cache in your config settings if you plan to
+			// use the cached message entity. 
+			var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			_client = new DiscordSocketClient(_config);
+
+			Console.WriteLine("Getting client");
+			await _client.LoginAsync(TokenType.Bot, _token);
+			await _client.StartAsync();
+
+			_commandService = new CommandService();
+			_commandHandler = new CommandHandler(_client, _commandService);
+			_commandHandler.InstallCommandsAsync();
+
+			_client.MessageUpdated += MessageUpdated;
+			_client.Ready += () =>
+			{
+				Console.WriteLine("Bot is connected!");
+				return Task.CompletedTask;
+			};
+
+			await Task.Delay(-1);
+		}
+
+		private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+		{
+			// If the message was not in the cache, downloading it will result in getting a copy of `after`.
+			var message = await before.GetOrDownloadAsync();
+			Console.WriteLine($"{message} -> {after}");
+		}
+	}
+}
---
+++ SampleDiscordBot/SampleDiscordBot/TestCommands.cs
@@ -0,0 +1,50 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDiscordBot
+{
+	//These should be dummy commands with no real access to anything for testing/learning
+	//All commands need to be public and extend ModuleBase
+	public class TestCommands
+	{
+		public class TestModule : ModuleBase<SocketCommandContext>
+		{
+			[Command("echo")]
+			[Summary("returns what you say")]
+			public async Task SimpleEcho([Summary("what you say")] string receivedText)
+			{
+				await ReplyAsync(receivedText);
+			}
+
+			[Command("username")]
+			[Summary("returns your username")]
+			public async Task SimpleUsername()
+			{
+				await ReplyAsync(Context.User.Username);
+			}
+		}
+
+		[Group("testing")]
+		public class TestGroupModule : ModuleBase<SocketCommandContext>
+		{
+			//this command since in a group will be accessed by doing "-testing echo"
+			[Command("echo")]
+			[Summary("returns what you say")]
+			public async Task SimpleTestingEcho([Summary("what you say")] string receivedText)
+			{
+				await ReplyAsync(receivedText);
+			}
+
+			[Command("channelname")]
+			[Summary("returns the channel name")]
+			public async Task SimpleTestingChannelName()
+			{
+				await ReplyAsync(Context.Channel.Name);
+			}
+		}
+	}
+}
